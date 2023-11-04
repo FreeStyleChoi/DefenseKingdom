a66974aafddb0856dc7510e6c3c9268bf57276a9
suyoung <suyoung@DESKTOP-SBEEDLG>
2023-11-04 09:57:20 +0900

    made window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
+	int window_w = 1460;
+	int window_h = 960;
+
+	const int FPS = 80;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Defense Kingdom", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_w, window_h, false);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	}
+
+	bool isRunning = true;
+	SDL_Event event;
+
+	while (isRunning)
+	{
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	
 	return 0;
 }
