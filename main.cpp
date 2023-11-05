8497da353facd5cab1284541b6c20915a251d873
suyoung <suyoung@DESKTOP-SBEEDLG>
2023-11-05 10:21:14 +0900

    add FPS, render, update

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 
 int main(int argc, char** argv)
 {
+	// INIT
 	int window_w = 1460;
 	int window_h = 960;
-
-	const int FPS = 80;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Defense Kingdom", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_w, window_h, false);
@@ -17,23 +16,39 @@ int main(int argc, char** argv)
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	}
 
+	// FPS SETTING
+	const int FPS = 100; // !!!!!FPS_MAX = 100!!!!! //
+	const int frameDelay = 1000 / FPS;
+	Uint32 frameStart;
+	Uint32 frameTime;
 
+	// LOAD IMAGES AND DEFINE IMAGE RECT
 	SDL_Surface* tmpSurface;
 
-
 	tmpSurface = IMG_Load("assets/Blue.png");
-	SDL_Texture* BlueTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_Texture* TmpTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	SDL_FreeSurface(tmpSurface);
 
-	SDL_Rect BlueRect{};
-	BlueRect.w = 64;
+	SDL_Rect TmpRect{};
+	TmpRect.w = 64;
+	TmpRect.h = 64;
+	TmpRect.x = 0;
+	TmpRect.y = 0;
+	double TmpRect_Speed = 0.1; // !!!!! SPEED_MIN = 1/frameDelay !!!!! //
 
+	short int TmpRect_XDir = 1;
+	short int TmpRect_YDir = 1;
 
+	// MAIN LOOP
 	bool isRunning = true;
 	SDL_Event event;
 
 	while (isRunning)
 	{
+		// fps 1
+		frameStart = (Uint32)SDL_GetTicks64();
+
+		// event
 		SDL_PollEvent(&event);
 		switch (event.type)
 		{
@@ -42,6 +57,22 @@ int main(int argc, char** argv)
 			break;
 		default:
 			break;
+		}
+
+		// update
+		TmpRect.x += (int)(TmpRect_Speed * frameDelay);
+		TmpRect.y += (int)(TmpRect_Speed * frameDelay);
+
+		// render
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, TmpTex, NULL, &TmpRect);
+		SDL_RenderPresent(renderer);
+
+		// fps 2
+		frameTime = (Uint32)SDL_GetTicks64() - frameStart;
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
 		}
 	}
 
