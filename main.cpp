cfe9144ff43c4857bc45f63fab233e1157334d32
Seungje Choi <seungje@gmail.com>
2023-11-06 19:53:35 +0900

    update TowerBusy logic

--- main.cpp
+++ main.cpp
@@ -13,15 +13,19 @@ float GetDistance(int x, int y);
 
 int main(int argc, char** argv)
 {
-	int window_w = 1460;
-	int window_h = 960;
+	int window_w = 800;
+	int window_h = 600;
 
 	const int FPS = 50;
 	const int frameDelay = 1000 / FPS;
 	Uint32 frameStart, frameTime;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Defense Kingdom", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_w, window_h, false);
+	SDL_Window* window = SDL_CreateWindow(
+		"Defense Kingdom", 
+		0, 
+		0, 
+		window_w, window_h, false);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	bool isRunning = true;
@@ -34,6 +38,8 @@ int main(int argc, char** argv)
 	SDL_Texture* backgroundTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	tmpSurface = IMG_Load("assets/green.png");
 	SDL_Texture* TowerTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	tmpSurface = IMG_Load("assets/gray.png");
+	SDL_Texture* TowerTextureBusy = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	tmpSurface = IMG_Load("assets/orenge.png");
 	SDL_Texture* EnemyTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	tmpSurface = IMG_Load("assets/purple.png");
@@ -52,7 +58,7 @@ int main(int argc, char** argv)
 	bool bTowerOnScreen[MAX_TOWERS]{};
 	bool bEnemyOnScreen[MAX_ENEMIES]{};
 	bool bBulletOnScreen[MAX_BULLETS]{};
-	bool bTowerBusy[MAX_TOWERS]{};
+	int nTowerBusy[MAX_TOWERS]{};
 	
 
 
@@ -61,7 +67,6 @@ int main(int argc, char** argv)
 		bTowerOnScreen[i] = true;
 	}
 
-	int nPad = 300;
 	Uint32 format;
 	int access;
 	int w;
@@ -70,8 +75,8 @@ int main(int argc, char** argv)
 	SDL_QueryTexture(TowerTexture, &format, &access, &w, &h);
 	for (int i = 0; i < MAX_TOWERS; i++)
 	{
-		TowerRect[i].x = (i+1)*400;
-		TowerRect[i].y = 200;
+		TowerRect[i].x = (i+1)*350;
+		TowerRect[i].y = 150;
 		TowerRect[i].w = w;
 		TowerRect[i].h = h;
 	}
@@ -93,14 +98,13 @@ int main(int argc, char** argv)
 	// End of Rectangle
 
 	// dummy variables
-	int nCount = 0;
 	int nSpeed = 20;
 	Uint32 nCountFrame = 0;
 	int nEnemyRate = 100;
 	int nEnemyIdx = 0;
-	int nFireRate = 50;
+	int nFireRate = 3;
 	int nBulletIdx = 0;
-	int nMaxRange = 400;
+	int nMaxRange = 300;
 
 	// Main Loop
 	while (isRunning)
@@ -152,7 +156,7 @@ int main(int argc, char** argv)
 			}
 		}
 
-		// Check Enemy condition
+		// Check collision between Enemy and bullet
 		for (int i = 0; i < MAX_ENEMIES; i++)
 		{
 			if (bEnemyOnScreen[i])
@@ -175,23 +179,14 @@ int main(int argc, char** argv)
 		// fire Bullet on Tower
 		for (int i = 0; i < MAX_TOWERS; i++)
 		{
-			if (nCountFrame % nFireRate == 0)
+			// Reset a rest time of Tower
+			if (nTowerBusy[i] > 0)
 			{
-				bTowerBusy[i] = false;
+				nTowerBusy[i]--;
 			}
 
-			if (bTowerOnScreen[i] && !bTowerBusy[i])
+			if (bTowerOnScreen[i] && nTowerBusy[i] == 0)
 			{
-				// find empty magazine and fire bullet
-				for (int k = 0; k < MAX_BULLETS; k++)
-				{
-					if (bBulletOnScreen[k] == false)
-					{
-						nBulletIdx = k;
-						bBulletOnScreen[nBulletIdx] = true;
-						break;
-					}
-				}
 				// find a Nearest Enemy
 				float fDistance1 = 2000.;
 				int nNearestEnemyIdx = 0;
@@ -202,17 +197,28 @@ int main(int argc, char** argv)
 						float fDistance2 = GetDistance(&TowerRect[i], &EnemyRect[j]);
 						if (fDistance2 > nMaxRange)
 						{
-							break;
+							continue;
 						}
 						if (fDistance1 > fDistance2)
 						{
+							// find empty magazine and fire bullet
+							for (int k = 0; k < MAX_BULLETS; k++)
+							{
+								if (bBulletOnScreen[k] == false)
+								{
+									nBulletIdx = k;
+									bBulletOnScreen[nBulletIdx] = true;
+									break;
+								}
+							}
+
 							fDistance1 = fDistance2;
 							nNearestEnemyIdx = j;
-							BulletDir[nBulletIdx].x = EnemyRect[j].x - TowerRect[i].x;
-							BulletDir[nBulletIdx].y = EnemyRect[j].y - TowerRect[i].y;
+							BulletDir[nBulletIdx].x = EnemyRect[j].x + EnemyRect[j].w / 2 - TowerRect[i].x - TowerRect[i].w / 2;
+							BulletDir[nBulletIdx].y = EnemyRect[j].y + EnemyRect[j].h / 2 - TowerRect[i].y - TowerRect[i].h / 2;
 							BulletRect[nBulletIdx].x = TowerRect[i].x + TowerRect[i].w / 2 - BulletRect[nBulletIdx].w / 2;
 							BulletRect[nBulletIdx].y = TowerRect[i].y + TowerRect[i].h / 2 - BulletRect[nBulletIdx].h / 2;
-							bTowerBusy[i] = true;
+							nTowerBusy[i] = nFireRate;
 						}
 					}
 				}
@@ -238,7 +244,14 @@ int main(int argc, char** argv)
 		{
 			if (bTowerOnScreen[i])
 			{
-				SDL_RenderCopy(renderer, TowerTexture, NULL, &TowerRect[i]);
+				if (nTowerBusy[i] == 0)
+				{
+					SDL_RenderCopy(renderer, TowerTexture, NULL, &TowerRect[i]);
+				}
+				else
+				{
+					SDL_RenderCopy(renderer, TowerTextureBusy, NULL, &TowerRect[i]);
+				}
 			}
 		}
 		for (int i = 0; i < MAX_ENEMIES; i++)
